24a0e64312cf1127c9aa405e5af5fef817c8e16f
barood-geek <aniruddha.1404@gmail.com>
2014-11-09 22:08:28 +0530

    this is an mvc branch commit

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,17 +1,16 @@
 package hello;
 
-import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
 
-@RestController
+@Controller
 public class GreetingController {
-	public static final String template = "Hello, %s";
-	private final AtomicLong counter = new AtomicLong();
 
 	@RequestMapping("/greeting")
-	public Greeting greeting(@RequestParam(value="name", defaultValue="world") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	public String greeting(@RequestParam(value="name", required=false, defaultValue="world") String name, Model model) {
+		model.addAttribute("name", name);
+		return "greeting";
 	}
 }
